d55148971577611a7a0ed239a51f6d48401e1057
Gwhyyy <ffikhi.aanas@gmail.com>
2023-01-30 04:39:52 +0100

    Update main.dart

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,7 @@
 import 'package:openai/src/instance/openai.dart';
 
 void main() async {
-  OpenAI.apiKey = "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";
+  OpenAI.apiKey = "YOUR KEY";
   final models = await OpenAI.instance.model.list();
   final model = await OpenAI.instance.model.one(models.first.id);
   print(model.id);
