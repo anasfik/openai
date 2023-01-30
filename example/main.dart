f782080eb2c7b8b0cfec40744476416d2383aa00
Gwhyyy <ffikhi.aanas@gmail.com>
2023-01-30 04:40:58 +0100

    [ Remove ] removed example folder

--- example/main.dart
+++ example/main.dart
@@ -2,6 +2,7 @@ import 'package:openai/src/instance/openai.dart';
 
 void main() async {
   OpenAI.apiKey = "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";
+  OpenAI.organization = "YOUR ORGANIZATION ID";
   final models = await OpenAI.instance.model.list();
   final model = await OpenAI.instance.model.one(models.first.id);
   print(model.id);
