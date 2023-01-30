c15b46f0894ab3a5851c8b40b6a99220df191598
Gwhyyy <ffikhi.aanas@gmail.com>
2023-01-30 01:57:03 +0100

    [ Add ] added initial project files

---
+++ lib/src/instance/openai.dart
@@ -0,0 +1,3 @@
+class OpenAI {
+  OpenAI._();
+}
---
+++ test/openai_test.dart
@@ -0,0 +1,8 @@
+import 'package:openai/openai.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
