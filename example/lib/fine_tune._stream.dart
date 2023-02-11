f3623668cafcdc80c53f8cfb8eda47650dad2b81
Gwhyyy <ffikhi.aanas@gmail.com>
2023-02-11 14:46:28 +0100

    [ Edit ] edited example to be reproducable

--- example/lib/fine_tune._stream.dart
+++ example/lib/fine_tune._stream.dart
@@ -11,22 +11,21 @@ void main() async {
   // Set the OpenAI API key from the .env file.
   OpenAI.apiKey = env['OPEN_AI_API_KEY']!;
 
-// // Upload an example File
-//   OpenAIFileModel file = await OpenAI.instance.file.upload(
-//     purpose: 'fine-tune',
-//     file: jsonLFileExample(),
-//   );
+// Upload an example File
+  OpenAIFileModel file = await OpenAI.instance.file.upload(
+    purpose: 'fine-tune',
+    file: jsonLFileExample(),
+  );
 
   // Creating a fine-tune job.
   OpenAIFineTuneModel fineTuneModel = await OpenAI.instance.fineTune.create(
     model: "ada",
-    trainingFile: "file-trITb8KgJfPnrohis0RsbIVH",
+    trainingFile: file.id,
   );
 
-
   // Get a stream of events for a fine-tune job.
-  Stream fineTuneEventStream = OpenAI.instance.fineTune
-      .listEventsStream(fineTuneModel.id);
+  Stream fineTuneEventStream =
+      OpenAI.instance.fineTune.listEventsStream(fineTuneModel.id);
 
   // Listen to the stream.
   fineTuneEventStream.listen((event) {
