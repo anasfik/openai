63661d4059280f8ad9187d6db6baf9a11ea07bb5
Gwhyyy <ffikhi.aanas@gmail.com>
2023-02-14 02:19:39 +0100

    [ Edit ] changed the documnattion for using the envied package

--- example/lib/env/env.dart
+++ example/lib/env/env.dart
@@ -3,7 +3,7 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied()
+@Envied(path: ".env")
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_API_KEY')
   static const apiKey = _Env.apiKey;
