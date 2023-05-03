74feddd3877368b53e4f370104e3b1c45f072cda
Anas Fikhi <work@gwhyyy.com>
2023-05-04 00:33:52 +0100

    [ Add ] adde sentence completion example, Added 1 word completion exmaple

---
+++ example/lib/chat_stream_example.dart
@@ -0,0 +1,30 @@
+import 'package:dart_openai/openai.dart';
+
+import 'env/env.dart';
+
+void main() async {
+  OpenAI.apiKey = Env.apiKey;
+
+  final chatStream = OpenAI.instance.chat.createStream(
+    model: "gpt-3.5-turbo",
+    messages: [
+      OpenAIChatCompletionChoiceMessageModel(
+        content: "Hello, can you say: 'You are Anas'",
+        role: OpenAIChatMessageRole.user,
+      ),
+      OpenAIChatCompletionChoiceMessageModel(
+        content: "You are Anas",
+        role: OpenAIChatMessageRole.assistant,
+      ),
+      OpenAIChatCompletionChoiceMessageModel(
+        content:
+            "Now I want you to repeat it, but change the word 'Anas' to 'Mohamed'",
+        role: OpenAIChatMessageRole.user,
+      ),
+    ],
+  );
+
+  chatStream.listen((event) {
+    print(event.choices.first.delta.content);
+  });
+}
---
+++ example/lib/sentence_completion.dart
@@ -0,0 +1,25 @@
+import 'package:dart_openai/openai.dart';
+import 'package:example/env/env.dart';
+
+Future<void> main() async {
+  OpenAI.apiKey = Env.apiKey;
+  OpenAI.showLogs = false;
+
+  Stream<String> wordsOfSentenceToComplete =
+      Stream.fromIterable(["Hi ", "Edward", ", ", "I "]);
+
+  String accumulativeSentence = "";
+
+  await for (var word in wordsOfSentenceToComplete) {
+    accumulativeSentence += word;
+
+    final completion = await OpenAI.instance.completion.create(
+      model: "text-davinci-003",
+      prompt: accumulativeSentence,
+      maxTokens: 20,
+    );
+
+    print(
+        "completion for '$accumulativeSentence': ${completion.choices.first.text}");
+  }
+}
