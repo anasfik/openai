2203054104e5a78d0a7b5d0a2f8a274af17f40d7
wqj <wqjuser@gmail.com>
2023-11-06 20:24:25 +0800

    官方更新了模型列表返回值，修改代码兼容

--- lib/src/core/models/model/model.dart
+++ lib/src/core/models/model/model.dart
@@ -13,37 +13,36 @@ final class OpenAIModelModel {
   final String ownedBy;
 
   /// The permissions of the model.
-  // final List<OpenAIModelModelPermission>? permission;
+  final List<OpenAIModelModelPermission>? permission;
 
   @override
-  int get hashCode => id.hashCode ^ ownedBy.hashCode
-  // ^ permission.hashCode
-  ;
+  int get hashCode => id.hashCode ^ ownedBy.hashCode ^ permission.hashCode;
 
   /// This class is used to represent an OpenAI model.
   const OpenAIModelModel({
     required this.id,
     required this.ownedBy,
-    // required this.permission,
+    required this.permission,
   });
 
   /// This method is used to convert a [Map<String, dynamic>] object to a [OpenAIModelModel] object.
   factory OpenAIModelModel.fromMap(Map<String, dynamic> json) {
+    // 进行空检查，如果 'permission' 为 null，则使用空列表或者 null。
+    final permissionJson = json['permission'] as List?;
+    final permissions = permissionJson != null
+        ? permissionJson.map((e) => OpenAIModelModelPermission.fromMap(e as Map<String, dynamic>)).toList()
+        : null; // 或者使用 <OpenAIModelModelPermission>[] 代替 null，如果你想要一个空列表。
+
     return OpenAIModelModel(
-      id: json['id'],
-      ownedBy: json['owned_by'],
-      // permission: (json['permission']? as List)
-      //     .map((e) =>
-      //         OpenAIModelModelPermission.fromMap(e as Map<String, dynamic>))
-      //     .toList(),
+      id: json['id'] as String,
+      ownedBy: json['owned_by'] as String,
+      permission: permissions,
     );
   }
 
   @override
   String toString() =>
-      'OpenAIModelModel(id: $id, ownedBy: $ownedBy'
-          // ', permission: $permission'
-          ')';
+      'OpenAIModelModel(id: $id, ownedBy: $ownedBy, permission: $permission)';
 
   @override
   bool operator ==(covariant OpenAIModelModel other) {
@@ -51,8 +50,6 @@ final class OpenAIModelModel {
     final listEquals = const DeepCollectionEquality().equals;
 
     return other.id == id &&
-        other.ownedBy == ownedBy
-        // && listEquals(other.permission, permission)
-    ;
+        other.ownedBy == ownedBy && listEquals(other.permission, permission);
   }
 }
