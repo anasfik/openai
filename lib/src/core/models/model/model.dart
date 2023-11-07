fe0a1025f2e6ed791b0a79ea1311df587d7b2fdb
Andrii Syrokomskyi <signmotion@gmail.com>
2023-11-07 16:07:08 +0100

    Fixed the test errors (see below). All 28 tests passed.
    # 1
    test\openai_test.dart: models list models [E]
      type 'Null' is not a subtype of type 'List<dynamic>' in type cast
      package:dart_openai/src/core/models/model/model.dart 33:39  new OpenAIModelModel.fromMap
      package:dart_openai/src/instance/model/model.dart 44:53     OpenAIModel.list.<fn>.<fn>
      dart:_internal                                              ListIterable.toList
      package:dart_openai/src/instance/model/model.dart 44:69     OpenAIModel.list.<fn>
      package:dart_openai/src/core/networking/client.dart 130:24  OpenAINetworkingClient.get
      ===== asynchronous gap ===========================
      package:dart_openai/src/instance/model/model.dart 37:12     OpenAIModel.list
      ===== asynchronous gap ===========================
      test\openai_test.dart 57:13                                 main.<fn>.<fn>
    
    # 2
    00:01 +3 -2: test\openai_test.dart: models retrieve a model [E]
      'file:///C:/projects/openai/test/openai_test.dart': Failed assertion: line 74 pos 9: 'modelExampleId != null': please set a model id that is not null, or let the previous test run first to get a model id example
      dart:core                   _AssertionError._throwNew
      test\openai_test.dart 74:9  main.<fn>.<fn>

--- lib/src/core/models/model/model.dart
+++ lib/src/core/models/model/model.dart
@@ -1,6 +1,4 @@
-import 'package:collection/collection.dart';
 import 'package:meta/meta.dart';
-import 'sub_models/permission.dart';
 
 export 'sub_models/permission.dart';
 
@@ -12,17 +10,13 @@ final class OpenAIModelModel {
   /// The name of the organization that owns the model.
   final String ownedBy;
 
-  /// The permissions of the model.
-  final List<OpenAIModelModelPermission> permission;
-
   @override
-  int get hashCode => id.hashCode ^ ownedBy.hashCode ^ permission.hashCode;
+  int get hashCode => id.hashCode ^ ownedBy.hashCode;
 
   /// This class is used to represent an OpenAI model.
   const OpenAIModelModel({
     required this.id,
     required this.ownedBy,
-    required this.permission,
   });
 
   /// This method is used to convert a [Map<String, dynamic>] object to a [OpenAIModelModel] object.
@@ -30,24 +24,14 @@ final class OpenAIModelModel {
     return OpenAIModelModel(
       id: json['id'],
       ownedBy: json['owned_by'],
-      permission: (json['permission'] as List)
-          .map((e) =>
-              OpenAIModelModelPermission.fromMap(e as Map<String, dynamic>))
-          .toList(),
     );
   }
 
   @override
-  String toString() =>
-      'OpenAIModelModel(id: $id, ownedBy: $ownedBy, permission: $permission)';
+  String toString() => 'OpenAIModelModel(id: $id, ownedBy: $ownedBy)';
 
   @override
-  bool operator ==(covariant OpenAIModelModel other) {
-    if (identical(this, other)) return true;
-    final listEquals = const DeepCollectionEquality().equals;
-
-    return other.id == id &&
-        other.ownedBy == ownedBy &&
-        listEquals(other.permission, permission);
-  }
+  bool operator ==(covariant OpenAIModelModel other) => identical(this, other)
+      ? true
+      : other.id == id && other.ownedBy == ownedBy;
 }
