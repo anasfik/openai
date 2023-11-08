6b875880c9e6c71c13822f638ea5841a2f9c71ba
Anas Fikhi <work@gwhyyy.com>
2023-11-08 19:34:58 +0100

    [ Edit ] returned back the web interface of the http client for js (web)

--- lib/src/core/networking/client.dart
+++ lib/src/core/networking/client.dart
@@ -4,7 +4,7 @@ import "dart:io";
 import "package:dart_openai/src/core/utils/extensions.dart";
 // ignore: unused_import
 import "package:dart_openai/src/core/utils/http_client_web.dart"
-    if (dart.library.js) "package:dart_openai/src/core/utils/http_client_io.dart"
+    if (dart.library.js) "package:dart_openai/src/core/utils/http_client_web.dart"
     if (dart.library.io) "package:dart_openai/src/core/utils/http_client_io.dart";
 
 import 'package:dart_openai/dart_openai.dart';
