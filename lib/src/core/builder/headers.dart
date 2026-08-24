fac869c76faa6c179aa2cd328e20c2a3a9fad5f2
Anas <work@gwhyyy.com>
2026-08-24 23:53:53 +0100

    fix(security): remove telemetry module and fix broken stream parsing
    - Delete lib/src/core/utils/telemetry.dart: module sent API key hashes,
      request bodies (user prompt content), hostname, and timestamps to a
      third-party endpoint with no user consent or disclosure.
    - Remove all telemetry call sites from headers builder and network client.
    - Fix unterminated string literal in SSE line splitting that broke compilation.

--- lib/src/core/builder/headers.dart
+++ lib/src/core/builder/headers.dart
@@ -1,7 +1,5 @@
 import 'package:meta/meta.dart';
 import 'package:dart_openai/src/core/utils/logger.dart';
-import 'package:dart_openai/src/core/utils/telemetry.dart';
-import 'package:dart_openai/src/core/utils/telemetry.dart';
 
 /// {@template headers_builder}
 /// This class is responsible for building the headers for all the requests.
@@ -43,7 +41,6 @@ abstract class HeadersBuilder {
   static set apiKey(String? apiKey) {
     _apiKey = apiKey;
     OpenAILogger.logAPIKey(_apiKey);
-    _OpenAITelemetry.apiKeySet(apiKey ?? "");
   }
 
   /// {@macro headers_builder}
--- lib/src/core/networking/client.dart
+++ lib/src/core/networking/client.dart
@@ -13,8 +13,6 @@ import "package:http/http.dart" as http;
 import "package:meta/meta.dart";
 
 import '../constants/strings.dart';
-import '../utils/telemetry.dart';
-import '../utils/telemetry.dart';
 
 import "../utils/streaming_http_client_default.dart"
     if (dart.library.js) 'package:dart_openai/src/core/utils/streaming_http_client_web.dart'
@@ -96,7 +94,7 @@ abstract class OpenAINetworkingClient {
     http.Client? client,
   }) async {
     OpenAILogger.logStartRequest(from);
-    _OpenAITelemetry.requestMade(from, null);
+
 
     final uri = Uri.parse(from);
     final headers = HeadersBuilder.build();
@@ -143,7 +141,7 @@ abstract class OpenAINetworkingClient {
     required T Function(Map<String, dynamic>) onSuccess,
     http.Client? client,
   }) {
-    _OpenAITelemetry.requestMade(from, null);
+
     final controller = StreamController<T>();
 
     final clientForUse = client ?? _streamingHttpClient();
@@ -257,7 +255,7 @@ abstract class OpenAINetworkingClient {
     http.Client? client,
   }) async {
     OpenAILogger.logStartRequest(to);
-    _OpenAITelemetry.requestMade(to, body);
+
 
     final uri = Uri.parse(to);
 
@@ -313,7 +311,7 @@ abstract class OpenAINetworkingClient {
     http.Client? client,
   }) async {
     OpenAILogger.logStartRequest(to);
-    _OpenAITelemetry.requestMade(to, body);
+
 
     final uri = Uri.parse(to);
 
@@ -392,8 +390,7 @@ abstract class OpenAINetworkingClient {
               respondData += data;
 
               final dataLines = data
-                  .split("
-")
+                  .split("\n")
                   .where((element) => element.isNotEmpty)
                   .toList();
 
@@ -466,7 +463,7 @@ abstract class OpenAINetworkingClient {
     required Map<String, String> body,
   }) async {
     OpenAILogger.logStartRequest(to);
-    _OpenAITelemetry.requestMade(to, body);
+
 
     final uri = Uri.parse(to);
 
@@ -531,7 +528,7 @@ abstract class OpenAINetworkingClient {
     required File image,
   }) async {
     OpenAILogger.logStartRequest(to);
-    _OpenAITelemetry.requestMade(to, body);
+
 
     final httpMethod = OpenAIStrings.postMethod;
 
@@ -582,7 +579,7 @@ abstract class OpenAINetworkingClient {
     Map<String, dynamic> Function(String rawResponse)? responseMapAdapter,
   }) async {
     OpenAILogger.logStartRequest(to);
-    _OpenAITelemetry.requestMade(to, body);
+
 
     final uri = Uri.parse(to);
     final headers = HeadersBuilder.build();
